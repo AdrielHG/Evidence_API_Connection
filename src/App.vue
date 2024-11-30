dbe7b30d2eb454338b21fc4912fe3922b07e2de9
AdrielHG <101303036+AdrielHG@users.noreply.github.com>
2024-11-30 01:34:28 -0600

    First form to display invoices

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,60 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      id: "",
+      note: {}
+    };
+  },
+  methods: {
+    fetchNote() {
+      if (this.id) {
+        axios.get(`http://127.0.0.1:8000/api/note/${this.id}`)
+          .then(response => {
+            this.note = response.data;
+          })
+          .catch(error => {
+            console.error("Error fetching note:", error);
+            this.note = { error: "Note not found. Please check the ID and try again." };
+          });
+      }
+    }
+  }
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+<div class="container mt-5">
+    <h2 class="text-center">Note Content by ID</h2>
+    <div class="row justify-content-center">
+      <div class="col-md-6">
+        <form id="searchForm" class="form-inline" @submit.prevent="fetchNote">
+          <div class="mb-3">
+            <label for="idInput" class="form-label">Note ID:</label>
+            <input type="text" v-model="id" class="form-control" id="idInput" placeholder="Enter ID" required>
+          </div>
+          <button type="button" class="btn btn-primary" @click="fetchNote">Search</button>
+        </form>
+      </div>
     </div>
-  </header>
+    <div class="row justify-content-center mt-4" v-if="note">
+      <div class="col-md-6">
+        <div id="result" class="alert alert-info">
+          <p v-if="note.id">Note ID: {{ note.id }}</p>
+          <p v-if="note.title">Title: {{ note.title }}</p>
+          <p v-if="note.content">Content: {{ note.content }}</p>
+          <p v-if="note.error" class="text-danger">{{ note.error }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
